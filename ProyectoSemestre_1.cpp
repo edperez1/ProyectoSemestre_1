52ca2a1a033a22f888ff893a65a7014c01e283b1
Farioo__ <165834695+edperez1@users.noreply.github.com>
2024-06-22 20:26:11 -0600

    arregle el codigo

--- ProyectoSemestre_1.cpp
+++ ProyectoSemestre_1.cpp
@@ -2,19 +2,22 @@
 #include <string>
 #include <vector>
 #include <conio.h>
+#include <limits>
 /*Este es nuestro Proyecto Final de el Primer semestre de la clase de Logica y Algoritmos */
 using namespace std;
-
 struct PaqueteInternet {
     string velocidad;
     string descripcion;
     double precio;
 };
 struct Cliente {
-    int id;
     string nombre;
+    string apellido;
     string direccion;
-    string telefono;
+    int telefono;
+    string cedula;
+    string Departamento;
+    
 };
 struct Factura {
     int numero_factura;
@@ -32,11 +35,41 @@ void agregar_cliente() {
     cout << "Ingrese el nombre del cliente: ";
     cin.ignore();
     getline(cin, nuevo_cliente.nombre);
-    cout << "Ingrese la dirección del cliente: ";
+    cout<<"Ingrese el apellido de el cliente";
+    getline(cin,nuevo_cliente.apellido);
+    cout<<"Ingrese el Departamento de el cliente: ";
+    getline(cin, nuevo_cliente.Departamento);
+    cout << "Ingrese la direccion del cliente: ";
     getline(cin, nuevo_cliente.direccion);
-    cout << "Ingrese el teléfono del cliente: ";
-    getline(cin, nuevo_cliente.telefono);
 
+    // Revisamos que el telefono que ingrese sea un numero
+    cout << "Ingrese el telefono del cliente : ";
+    while (true) {
+    if (cin >> nuevo_cliente.telefono) {
+        if (to_string(nuevo_cliente.telefono).length() == 8) {
+            break;  // Sale del bucle si se ingresaron 8 dígitos
+        } else {
+            cout << "Ingrese un Numero Nacional ";
+        }
+    } else {
+        cout << "Ingrese Un Numero  ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
+
+// Revisamos que la cédula que ingrese tenga exactamente 14 dígitos
+cout << "Introduzca la cedula de el cliente : ";
+while (true) {
+    cin >> nuevo_cliente.cedula;
+    if (nuevo_cliente.cedula.length() == 14) {
+        break;  // Sale del bucle si se ingresaron 9 dígitos
+    } else {
+        cout << "Intente de nuevo ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
     clientes.push_back(nuevo_cliente);
     cout << "Cliente agregado con éxito." << endl;
 }
@@ -50,8 +83,8 @@ Factura crear_factura(int numero_factura, const string& fecha, double total, con
     return factura;
 }
 void MenuOpciones(){
-    cout<<"-----Menu Principal-----"<<endl;
-    cout << "\n1.Menu Para Empleados"  << "\n2.Menu para Clientes"<< "\n3.Salir del Programa"<<endl;
+    cout<<"                   -----Menu Principal-----"<<endl;
+    cout << "\n1.Menu de Empresa"  << "\n2.Menu para Clientes"<< "\n3.Salir del Programa"<<endl;
 }
 
 void mostrarPrecios() {
@@ -68,7 +101,7 @@ void mostrarInformacionPaquete(const PaqueteInternet& paquete) {
 }
 
 void mostrar_menu() {
-    cout << "\n-- Menu de Empleados--" << endl;
+    cout << "\n-- Menu de Empresa--" << endl;
     cout << "1. Agregar Cliente" << endl;
     cout << "2. Mostrar Clientes" << endl;
     cout << "3. Eliminar clientes"<<endl;
@@ -169,7 +202,7 @@ do {
 
 }
 int main() {
-    
+
     MostrarMenuOpcion();
   
     return 0;
